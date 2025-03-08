c853ff7998126053e3be96a8fff1e0cb9bf819ee
Mdimran19 <128302897+Mdimran19@users.noreply.github.com>
2025-03-08 13:00:12 +0600

    Update BlogList.jsx

--- client/src/user/components/BlogList.jsx
+++ client/src/user/components/BlogList.jsx
@@ -17,7 +17,7 @@ const BlogList = () => {
     const fetchBlogs = async () => {
       try {
 
-        const response = await axios.get('https://blogworld-client.onrender.com//blog/blog-get');
+        const response = await axios.get('https://blogworld-client.onrender.com/blog/blog-get');
         if (response && response.data) {
           setBlogs(response.data); // Set blogs data
         } else {
