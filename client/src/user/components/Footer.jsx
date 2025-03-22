61e2b403da61169f806f38463b8f8a01c9f65abf
Mdimran19 <128302897+Mdimran19@users.noreply.github.com>
2025-03-23 01:49:39 +0600

    Update Footer.jsx

--- client/src/user/components/Footer.jsx
+++ client/src/user/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'react-router-dom'
 const Footer = () => {
   return (
     <div>
@@ -13,9 +13,9 @@ const Footer = () => {
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400  mx-[10%] mt-12 py-6'>
             <p>@<span className='px-3'>2025.</span> <span>IMRAN</span> All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/loginuser">Login</a></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+                <li><Link to="/register">Register</Link ></li>
+                <li><Link to="/loginuser">Login</Link></li>
             </ul>
         </div>
     </div>
